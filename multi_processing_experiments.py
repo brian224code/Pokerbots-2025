859442a41aaade8876f7f3061f0c244d0e8059d4
Brian Le <brianle@mit.edu>
2025-01-21 00:19:45 -0500

    Finished experimental parallel program

--- multi_processing_experiments.py
+++ multi_processing_experiments.py
@@ -1,65 +1,76 @@
 import multiprocessing as mp
+import queue
+import os
 from time import sleep
 from random import random
 
 class Trainer:
     def __init__(self):
-        self.data = {
-            0: 0,
-            1: 0
-        }
-        self.locks = None
+        self.data = {}
+        self.locks = {}
 
-    def algo(self, player, t):
-        player = 1
+    def algo(self, player, t, q):
         sleep(random())    
+        if player not in self.locks:
+            q.put(player)
+            sleep(random())
         with self.locks[player]:
             sleep(random())
+            if player not in self.data:
+                self.data[player] = 0
             self.data[player] += 1
         print(f'Player {player} says hello from iter {t}')
 
-    def solve(self, iters, multi_processing=True):
-        num_cores = mp.cpu_count()
-        if multi_processing and num_cores > 1:
-            print(f'Setting up dict managers...')
-            manager = mp.Manager()
-            self.data = manager.dict(self.data)
-            self.locks = manager.dict({
-                0: manager.Lock(),
-                1: manager.Lock()
-            })
+    def solve(self, iters, players=2):
+        num_cores = os.process_cpu_count()
+        print(f'Setting up dict managers...')
+        manager = mp.Manager()
+        self.data = manager.dict(self.data)
+        self.locks = manager.dict(self.locks)
+        q = mp.Queue(maxsize=1)
+        
 
-            print(f'Parallel training with {num_cores} cpu cores...')
-            parallelization_factor = num_cores // 2
-            for iter in range(0, iters, parallelization_factor):
-                processes = [
-                    mp.Process(target=self.algo, args=(player, t)) 
-                    for player in [0, 1]
-                    for t in range(iter, min(iters, iter + parallelization_factor))
-                ]
+        print(f'Parallel training with {num_cores} cpu cores...')
+        parallelization_factor = num_cores // players
+        for iter in range(0, iters, parallelization_factor):
+            processes = [
+                mp.Process(target=self.algo, args=(player, t, q)) 
+                for player in range(players)
+                for t in range(iter, min(iters, iter + parallelization_factor))
+            ]
 
-                print('Starting processes...')
-                for process in processes:
-                    process.start()
+            print('Starting processes...')
+            for process in processes:
+                process.start()
 
-                print('Waiting for processes...')
-                for process in processes:
-                    process.join()
+            print('Waiting for processes...')
 
-                print('Done with one cycle...')
+            while any(map(lambda process: process.is_alive(), processes)):
+                try:
+                    new_bucket = q.get(block=False)
+                    print(f'Adding new bucket: {new_bucket}')
+                    self.locks[new_bucket] = manager.Lock()
+                except queue.Empty:
+                    pass
 
-            print(f'Closing dict managers...')
-            self.data = dict(self.data)
-            self.locks = None
+            print('Done with one cycle...')
 
-        else:
-             print('Training sequentially...')
-             for t in range(iters):
-                for player in [0, 1]:
-                    self.algo(player, t)
+        print(f'Closing dict managers...')
+        self.data = dict(self.data)
+        self.locks = dict(self.data)
+        q.close()
 
 
 if __name__ == '__main__':
-    trainer = Trainer()
-    trainer.solve(100)
-    print(trainer.data)
+    trials = 10
+    iters = 100
+    buckets = 8
+    correct = 0
+    for trial in range(trials):
+        print('=======================================================')
+        print(f'Starting trial {trial}...')
+        trainer = Trainer()
+        trainer.solve(iters, buckets)
+        if all(map(lambda bucket: bucket == iters, trainer.data.values())):
+            correct += 1
+    print(f'Overall correctness: {float(correct) / trials}')
